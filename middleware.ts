5c072209b182f29fdd4cb4794ed1e70ca04446fe
Osama_Anbari <osama.3nbri13@gmail.com>
2025-07-21 00:30:44 +0300

    locale

--- middleware.ts
+++ middleware.ts
@@ -4,18 +4,6 @@ import { i18n } from "@/lib/i18n-config"
 import Negotiator from "negotiator"
 import { match as matchLocale } from "@formatjs/intl-localematcher"
 
-function getLocale(request: NextRequest): string {
-  // Negotiator expects plain object so we need to transform headers
-  const negotiatorHeaders: Record<string, string> = {}
-  request.headers.forEach((value, key) => (negotiatorHeaders[key] = value))
-
-  // Use negotiator and intl-localematcher to get best locale
-  const languages = new Negotiator({ headers: negotiatorHeaders }).languages()
-  // @ts-ignore locales are readonly
-  const locales: string[] = i18n.locales
-  return matchLocale(languages, locales, i18n.defaultLocale)
-}
-
 export function middleware(request: NextRequest) {
   const pathname = request.nextUrl.pathname
 
@@ -26,7 +14,7 @@ export function middleware(request: NextRequest) {
 
   // Redirect if there is no locale
   if (pathnameIsMissingLocale) {
-    const locale = getLocale(request)
+    const locale = 'ar'
 
     // e.g. incoming request is /products
     // The new URL is now /en/products
